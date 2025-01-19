263ff943806d22724fa9b25862809e5c93e7661c
Phùng Anh Minh <dieglevel@gmail.com>
2025-01-19 16:59:38 +0700

    Add: extend Tailwind CSS configuration with custom colors and box shadows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,34 @@ export default {
   theme: {
   	extend: {
   		colors: {
+			colorbrand: {
+				burntSienna: {
+					50: '#fef4f2',
+					100: '#fee7e2',
+					200: '#ffd3c9',
+					300: '#fdb5a4',
+					400: '#f98a70',
+					500: '#f27052',
+					600: '#de4724',
+					700: '#bb381a',
+					800: '#9a321a',
+					900: '#802f1c',
+					950: '#461409'
+				},
+				midnightBlue: {
+					50: '#eef8ff',
+					100: '#dcf1ff',
+					200: '#b2e4ff',
+					300: '#6dcfff',
+					400: '#20b7ff',
+					500: '#009eff',
+					600: '#007ddf',
+					700: '#0063b4',
+					800: '#005495',
+					900: '#00457a',
+					950: '#00315c'
+				}
+			},
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
@@ -55,7 +83,17 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		boxShadow: {
+			DEFAULT: '0 0 0 1px hsl(var(--border))',
+			sm: '0 1px 2px 0 hsl(var(--shadow))',
+			md: '0 4px 6px -1px hsl(var(--shadow)), 0 2px 4px -1px hsl(var(--shadow))',
+			lg: '0 10px 15px -3px hsl(var(--shadow)), 0 4px 6px -2px hsl(var(--shadow))',
+			xl: '0 20px 25px -5px hsl(var(--shadow)), 0 10px 10px -5px hsl(var(--shadow))',
+			'2xl': '0 25px 50px -12px hsl(var(--shadow))',
+			inner: 'inset 0 2px 4px 0 hsl(var(--shadow))',
+			none: 'none'
+		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
