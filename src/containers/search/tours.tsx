9f1dfbadeef49152f6178d540c2b6c9f77a25b83
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2025-04-08 15:54:58 +0700

    feat: enhance search sidebar with filter functionality and improve tour display logic

--- src/containers/search/tours.tsx
+++ src/containers/search/tours.tsx
@@ -7,19 +7,16 @@ import ReactStars from 'react-stars'
 import api from '@/libs/axios/axios.config'
 import { BaseResponse } from '@/types'
 import { ITourEntity } from '@/models/response/tour'
+import { FORMAT_MONEY } from '@/utils/formatMoney'
 
 export const ResultComponent = () => {
   const [clientTour, setClientTour] = useState<BaseResponse<ITourEntity[]>>()
 
-  const formatPrice = (price: number) => {
-    return new Intl.NumberFormat('vi-VN').format(price)
-  }
-
   const getTour = async () => {
     try {
       const res = await api.get(`${process.env.NEXT_PUBLIC_BOOKING_SERVICE}/tours`)
-      if (res.data.data) {
-        return res.data
+      if (res.data) {
+        setClientTour(res.data)
       }
     } catch (error) {
       console.error('Lỗi khi gọi API lấy tour:', error)
@@ -28,19 +25,17 @@ export const ResultComponent = () => {
 
   useEffect(() => {
     const fetchTour = async () => {
-      const tours = await getTour()
-      console.log(tours)
-      setClientTour(tours)
+      await getTour()
     }
     fetchTour()
   }, [])
 
   if (!clientTour) return null
   return (
-    <section className="mx-auto px-4 py-12">
+    <section className="mx-8">
       {/* tour */}
-      <div className="mt-8 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
-        {clientTour.data.slice(0, 8).map((tour) => {
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+        {clientTour.slice(0, 8).map((tour:ITourEntity) => {
           return (
             <div key={tour.tourId} className="group overflow-hidden">
               <Card className="rounded-md border-none bg-white/80 shadow-sm">
@@ -66,7 +61,7 @@ export const ResultComponent = () => {
                   </div>
                   <div className="my-4 w-full border bg-[#D1D1D1]"></div>
                   <div className="flex items-center justify-between">
-                    <span className="font-bold text-[#00315C]">{formatPrice(tour.price)} đ</span>
+                    <span className="font-bold text-[#00315C]">{FORMAT_MONEY(tour.price)} đ</span>
                     <Button className="border-orange-500 bg-white font-bold text-orange-500 hover:bg-orange-50">
                       Xem chi tiết
                     </Button>
