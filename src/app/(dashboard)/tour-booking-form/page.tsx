65e7451fe7a2fe9bb8b8534e1dc46cebe16bedbd
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2025-04-21 21:05:37 +0700

    add: payment

---
+++ src/app/(dashboard)/tour-booking-form/page.tsx
@@ -0,0 +1,9 @@
+import TourBookingForm from "@/containers/tourBooking/tour-booking-form";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <TourBookingForm />
+    </main>
+  )
+}
