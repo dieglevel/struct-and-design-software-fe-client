e2ad3f5f7f727f1303c55a8d4b0ab556a00cd3c4
Le Minh Quang <lmqiuhdev@gmail.com>
2025-05-15 07:54:18 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,6 @@ import { heroui } from '@heroui/theme'
 import type { Config } from 'tailwindcss'
 
 export default {
-  // darkMode: ['class'],
-  darkMode: false,
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
